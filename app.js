64c8b7d524aac1b6f093ef2adce779d219b53397
Pooh5159 <92722233+Pooh5159@users.noreply.github.com>
2022-02-01 00:05:53 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -85,7 +85,7 @@ mybutton = document.getElementById("toTop");
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+  if (document.body.scrollTop > 400 || document.documentElement.scrollTop > 400) {
     mybutton.style.display = "block";
   } else {
     mybutton.style.display = "none";
